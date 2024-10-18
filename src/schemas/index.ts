85aa07d96b2f607496c11964ada2eac9d0335450
juan <juan@shaper.us>
2024-10-18 10:59:43 -0300

    feat: index for each schema, & db syncronitation

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,5 +1,4 @@
-import mongoose from 'mongoose';
-import client from '../database/connection-db'; // Cambiado a client ya que ahora exporta una conexión.
+import client from '../database/connection-db'; 
 
 import productSchema from './schemas-folder/product';
 import userSchema from './schemas-folder/user';
@@ -9,7 +8,6 @@ import addressSchema from './schemas-folder/address';
 
 const con = client;
 
-// Modelos
 export const Product = con.model('Product', productSchema);
 export const User = con.model('User', userSchema);
 export const Order = con.model('Order', orderSchema);
