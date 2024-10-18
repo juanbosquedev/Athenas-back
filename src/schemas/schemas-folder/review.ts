a57e96f4e2dd2fb4140e58b3beacf909e2d920f9
juan <juan@shaper.us>
2024-10-18 11:11:49 -0300

    feat: review schema

--- src/schemas/schemas-folder/review.ts
+++ src/schemas/schemas-folder/review.ts
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 
-
 const reviewSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
