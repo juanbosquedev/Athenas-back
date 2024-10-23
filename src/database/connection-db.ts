0f7c3c7d401c13b54b6652f1e068dbbf43bfde97
Juan Bosque <86373520+juanbosquedev@users.noreply.github.com>
2024-10-23 09:42:40 -0300

    connection-db.ts
    - Conditional variable, for undefined case (ts. rule)
    
    Signed-off-by: Juan Bosque <86373520+juanbosquedev@users.noreply.github.com>

--- src/database/connection-db.ts
+++ src/database/connection-db.ts
@@ -1,6 +1,10 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 import { uri } from "../config/config";
 
+if (!uri) {
+  throw new Error("MongoDB connection URI is not defined in the environment variables.");
+}
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
