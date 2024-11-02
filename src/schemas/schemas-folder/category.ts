1b8c2b7abb2523c0413d8dac10b543255363a642
juan <juan@shaper.us>
2024-11-02 20:16:50 -0300

    feat: new color property into category schema

--- src/schemas/schemas-folder/category.ts
+++ src/schemas/schemas-folder/category.ts
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
 	name: { type: String, required: true },
-    parentId: { type: mongoose.Schema.Types.ObjectId, ref: "Category", default: null }, 
-    gender: { type: String, enum: ["Hombre", "Mujer", "Unisex"], default: null }, 
-    description: { type: String, default: "" },	
+	color: { type: String, required: true },
 });
 
 export default categorySchema;
