83560cc40ee94a7831644452f56cd7c609c1a213
juan <juan@shaper.us>
2024-11-02 16:20:54 -0300

    feat: schemas formatted by biome

--- src/schemas/schemas-folder/address.ts
+++ src/schemas/schemas-folder/address.ts
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const addressSchema = new mongoose.Schema({
-    street: { type: String, required: true },
-    country: { type: String, required: true },
-    city: { type: String, required: true },
-    zipCode: { type: String, required: true },
-    additionalInfo: { type: String },
-    default:{type: Boolean},
+	street: { type: String, required: true },
+	country: { type: String, required: true },
+	city: { type: String, required: true },
+	zipCode: { type: String, required: true },
+	additionalInfo: { type: String },
+	default: { type: Boolean },
 });
 
 export default addressSchema;
--- src/schemas/schemas-folder/product.ts
+++ src/schemas/schemas-folder/product.ts
@@ -1,12 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    category: { type: String },
-    stock: { type: Number, required: true },
-    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }]
+	name: { type: String, required: true },
+	description: { type: String, required: true },
+	price: { type: Number, required: true },
+	category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+	image: { type: String, required: true },
+	stock: { type: Number, required: true },
+	reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
 });
 
 export default productSchema;
