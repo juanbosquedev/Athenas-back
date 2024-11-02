e1963d3b1d07d15c64d1994cc410a466aeec2cfd
juan <juan@shaper.us>
2024-11-02 19:18:34 -0300

    feat: new category schemas props

--- src/schemas/schemas-folder/category.ts
+++ src/schemas/schemas-folder/category.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
-	name: { type: String, required: true, unique: true },
+	name: { type: String, required: true },
+    parentId: { type: mongoose.Schema.Types.ObjectId, ref: "Category", default: null }, 
+    gender: { type: String, enum: ["Hombre", "Mujer", "Unisex"], default: null }, 
+    description: { type: String, default: "" },	
 });
 
 export default categorySchema;
