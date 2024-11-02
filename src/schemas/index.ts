d71e2213cc12427d78cfc2941ffffe450ec966b9
juan <juan@shaper.us>
2024-11-02 16:21:52 -0300

    feat: category schema created

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,16 +1,17 @@
-import client from '../database/connection-db'; 
+import client from "../database/connection-db";
 
-
-import productSchema from './schemas-folder/product';
-import userSchema from './schemas-folder/user';
-import orderSchema from './schemas-folder/order';
-import reviewSchema from './schemas-folder/review';
-import addressSchema from './schemas-folder/address'; 
+import productSchema from "./schemas-folder/product";
+import userSchema from "./schemas-folder/user";
+import orderSchema from "./schemas-folder/order";
+import reviewSchema from "./schemas-folder/review";
+import addressSchema from "./schemas-folder/address";
+import categorySchema from "./schemas-folder/category";
 
 const con = client;
 
-export const Product = con.model('Product', productSchema);
-export const User = con.model('User', userSchema);
-export const Order = con.model('Order', orderSchema);
-export const Review = con.model('Review', reviewSchema);
-export const Address = con.model('Address', addressSchema);
+export const Product = con.model("Product", productSchema);
+export const User = con.model("User", userSchema);
+export const Order = con.model("Order", orderSchema);
+export const Review = con.model("Review", reviewSchema);
+export const Address = con.model("Address", addressSchema);
+export const Category = con.model("Category", categorySchema);
