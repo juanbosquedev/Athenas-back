6617d4c7c8c50477d3526bd756305f3b048065c2
juan <juan@shaper.us>
2024-11-12 10:01:53 -0300

    feat: router root for reviews

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import productsRouter from './products-router/index';
+import reviewRouter from "./review-router/index"
 
 const router = Router();
 
 router.use('/products', productsRouter);
+router.use('/reviews', reviewRouter);
+
 
 export default router; 
