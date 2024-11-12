7101441ea9b17dc3157b55fd9c413616033884e8
juan <juan@shaper.us>
2024-11-12 09:59:46 -0300

    feat: populate reviews into products

--- src/controllers/products/products-controller/all-products.ts
+++ src/controllers/products/products-controller/all-products.ts
@@ -3,7 +3,7 @@ import { Product } from "../../../schemas/index";
 
 async function allProducts(req: Request, res: Response): Promise<void> {
     try {
-        const products = await Product.find().populate("category");
+        const products = await Product.find().populate("category").populate("reviews");
         res.status(200).json(products);
     } catch (error) {
         console.error("Error fetching products:", error);
