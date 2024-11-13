fb9ee28ab3f11e35ebec3f9802e2e55c75d3ffab
juan <juan@shaper.us>
2024-11-13 09:50:55 -0300

    fix: space code tidy

---
+++ src/routes/product-router/index.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { allProducts, createProduct, getProductById, deleteProductById, updateProduct } from "../../controllers/products/index";
+import { createReview, getReviewsByProduct } from "../../controllers/reviews/index";
+
+const router = Router();
+
+router.get("/", allProducts);
+router.get("/:id", getProductById);
+router.post("/", createProduct);
+router.patch("/:id", updateProduct);
+router.delete("/:id", deleteProductById);
+
+router.get("/:productId/reviews", getReviewsByProduct);
+router.post("/:productId/reviews", createReview);
+
+export default router;
