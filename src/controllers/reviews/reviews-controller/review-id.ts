758c20b8c3b6b5a778f2894347e8338fc5432cc5
juan <juan@shaper.us>
2024-11-13 11:08:01 -0300

    feat: users reviews populated

--- src/controllers/reviews/reviews-controller/review-id.ts
+++ src/controllers/reviews/reviews-controller/review-id.ts
@@ -1,12 +1,13 @@
 import type { Request, Response } from "express";
-import mongoose from "mongoose";
 import { Review } from "../../../schemas/index";
 
-async function  getReviewsByProduct(req: Request, res: Response): Promise<void> {
+async function getReviewsByProduct(req: Request, res: Response): Promise<void> {
     try {
         const { productId } = req.params;
 
-        const reviews = await Review.find({ product: productId }).populate("user", "name");
+        const reviews = await Review.find({ product: productId })
+        .populate("user", "name")
+        .populate("product", "name description price");;
         res.json(reviews);
     } catch (error) {
         res.status(500).json({ error: "Error fetching reviews" });
--- src/controllers/users/users-controller/all-usesrs.ts
+++ src/controllers/users/users-controller/all-usesrs.ts
@@ -3,7 +3,7 @@ import { User } from "../../../schemas/index";
 
 async function getAllUsers(req: Request, res: Response): Promise<void> {
     try {
-        const users = await User.find().populate("addresses orders favoriteProducts reviews");
+        const users = await User.find().populate("addresses orders favoriteProducts reviews");;
         res.status(200).json(users);
     } catch (error) {
         console.error("Error fetching users:", error);
