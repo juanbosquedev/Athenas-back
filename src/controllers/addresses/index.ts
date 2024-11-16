b78989063d5527938b46ac6227d9696fa85d4a17
juan <juan@shaper.us>
2024-11-16 11:56:50 -0300

    fix: address' name controllers fixed

--- src/controllers/addresses/index.ts
+++ src/controllers/addresses/index.ts
@@ -1,6 +1,6 @@
 import { getUserAddresses } from "./addresses-controller/all-addresses";
-import { addAddress } from "./addresses-controller/create-user";
-import { deleteAddress } from "./addresses-controller/delete-user";
-import { updateAddress } from "./addresses-controller/update-user";
+import { addAddress } from "./addresses-controller/create-address";
+import { deleteAddress } from "./addresses-controller/delete-address";
+import { updateAddress } from "./addresses-controller/update-address";
 
 export { getUserAddresses, addAddress, deleteAddress, updateAddress };
